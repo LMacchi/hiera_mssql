ca001aea94da5c2c9a677271d3e5546cae1e8cd1
root <root@LV00348.test.corp.oncor.com>
2017-09-13 14:43:01 -0500

    Add more debug

--- lib/puppet/functions/mssql_lookup_key.rb
+++ lib/puppet/functions/mssql_lookup_key.rb
@@ -45,7 +45,7 @@ Puppet::Functions.create_function(:mssql_lookup_key) do
     end
     
     result = mssql_get(key, context, options)
-    Puppet.debug("Hiera-mssql: Returned #{result}")
+    Puppet.debug("Found #{result.length} results for #{key}")
 
     if result.empty?
       context.not_found
@@ -108,7 +108,7 @@ Puppet::Functions.create_function(:mssql_lookup_key) do
 
         res.each do |row|
            data[var] = row[value].chomp
-           Puppet.debug("Hiera-mssql: Adding {#{var}} to data with value {#{row[value]}}")
+           Puppet.debug("Hiera-mssql: Adding {#{var}} to data with value {#{row[value]}")
         end
 
         Puppet.debug("Hiera-mssql: Value found is #{data[var]}")
